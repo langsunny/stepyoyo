c73ce8c1294bb6b96974b4aa7659740e3d6532af
langsunny <etolang@gmail.com>
2018-01-17 15:17:34 +0800

    增加了主程序注释

--- scripts/main.ino
+++ scripts/main.ino
@@ -1,4 +1,13 @@
+/*
+Intro:
+基于arduino单片机驱动步进电机，步进电机驱动机械装置yoyo摇摆。步进电机型号28byj-48，5v版本，为5相四线电机，单片机不能直接驱动，电机功率较小，可以用uln2003驱动。
+author:langsunny
+date:Jan 18, 2018
+version: 1.0.2
+
+*/
 #include<Stepper.h>
+
  
 // 参考文件
 /*
@@ -14,7 +23,7 @@ Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
 void setup()
 {
     // 设置转速，单位r/min
-    myStepper.setSpeed(180);//摇摆器默认转速150
+    myStepper.setSpeed(180);//摇摆器默认转速150,Yoyo周期大约是1秒
  
     // 初始化串口
     Serial.begin(9600);
